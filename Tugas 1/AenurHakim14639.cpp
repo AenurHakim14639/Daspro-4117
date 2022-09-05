af81719892c848c5eabb6c28c3c9031f90a799e1
AenurHakim14639 <112880111+AenurHakim14639@users.noreply.github.com>
2022-09-05 22:43:35 +0700

    Add files via upload

---
+++ Tugas 1/AenurHakim14639.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Aenur Hakim" << endl;
+    cout << "Nim : A11.2022.14639" <<endl;
+    cout << "Asal Sekolah : SMA Negeri 1 Moga" <<endl;
+    return 0;
+}
