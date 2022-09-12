75d976ef9ac3312456120b0fb436611e9b049b3e
AenurHakim14639 <112880111+AenurHakim14639@users.noreply.github.com>
2022-09-12 21:52:05 +0700

    Add files via upload

---
+++ Tugas Pertemuan 3/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello!" << endl;
+
+    string nama;
+    cout << "Nama Anda :";
+    cin >> nama ;
+
+    string  NIM ;
+    cout << "NIM : ";
+    cin >> NIM ;
+
+    cout << "Hi " << nama ;
+    cout << " NIM " << NIM ;
+    cout << " Selamat Datang! \n" <<endl;
+
+    cout << "Aenur Hakim Maulia \nA11.2022.14639" <<endl;
+
+    int u = 18;
+    float t = 158;
+    char b = 'M';
+    float n = 14639;
+
+    cout <<"Umur : " <<u;
+    cout << endl;
+    cout <<"Tinggi : " <<t;
+    cout << endl;
+    cout <<"Ukuran Baju : " <<b;
+    cout <<"\nNIM : " <<n;
+    return 0;
+}
