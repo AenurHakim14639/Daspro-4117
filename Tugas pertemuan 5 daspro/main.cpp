3191ad222ddd3ae126149f1e78b8718b4da87de4
AenurHakim14639 <112880111+AenurHakim14639@users.noreply.github.com>
2022-09-19 22:24:14 +0700

    Add files via upload

---
+++ Tugas pertemuan 5 daspro/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "## TUGAS DASPRO PERTEMUAN 5 ##" << endl;
+    cout << "---Menghitung Luas Segitiga---" <<endl;
+    cout << "==============================" <<endl;
+    cout << endl;
+
+
+    cout << "Assalamualaikum.\nKONICHIWA!" << endl;
+
+    string nama;
+    cout << "Nama Anda :";
+    cin >> nama ;
+
+    cout << "Hi, Domo " << nama ;
+    cout << " -San!, sekarang kita akan mencoba menghitung Luas Segitiga ya! \n" <<endl;
+
+    float r, luas;
+
+    cout <<"Masukkan Jari-Jari Lingkaran: ";
+    cin >> r;
+    luas = 3.14 * r * r;
+
+    cout <<"Luas lingkaran: " << luas <<endl;
+
+    cout <<"\n@A11.2022.14639 Aenur Hakim Maulia \nARIGATOU GOZAIMASU!" <<endl;
+
+
+    return 0;
+
+
+}
