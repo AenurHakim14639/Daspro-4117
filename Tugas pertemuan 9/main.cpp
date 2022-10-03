a361bf48fde35dded19089864f8228863b932269
AenurHakim14639 <112880111+AenurHakim14639@users.noreply.github.com>
2022-10-03 21:10:44 +0700

    Add files via upload

---
+++ Tugas pertemuan 9/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x;
+    x = 0;
+    //Ganjil
+    do {
+            if (x % 5 == 0) {
+                 cout << x << endl;
+            }
+            x++;
+    }while(x <= 40);
+
+    return 0;
+}
